c319c7be6a05d0091a355f60757d2f562d03350b
cqqyd2014 <cqqyd2014@hotmail.com>
2018-12-01 16:53:21 +0800

    20181201数据初始化

---
+++ 数据关系可视化系统.mm
@@ -0,0 +1,28 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1543508912422" ID="ID_861451179" MODIFIED="1543508946569" TEXT="&#x6570;&#x636e;&#x5173;&#x7cfb;&#x53ef;&#x89c6;&#x5316;&#x7cfb;&#x7edf;">
+<node CREATED="1543508947397" ID="ID_967379690" MODIFIED="1543508957871" POSITION="right" TEXT="&#x6570;&#x636e;&#x91c7;&#x96c6;&#x5b58;&#x5165;HBASE">
+<node CREATED="1543570172658" ID="ID_409767727" MODIFIED="1543570181400" TEXT="&#x5bfc;&#x5165;&#x6846;&#x67b6;&#x8bbe;&#x8ba1;">
+<node CREATED="1543570208070" ID="ID_1497141179" MODIFIED="1543570223857" TEXT="&#x6838;&#x5fc3;&#x6a21;&#x5757;">
+<node CREATED="1543570223859" ID="ID_897139506" MODIFIED="1543627073786" TEXT="&#x5b58;&#x50a8;&#x6a21;&#x5f0f;&#xff08;&#x5217;&#xff09;&#xff0c;&#x4e0d;&#x540c;&#x6570;&#x636e;&#x7684;rowkey&#x548c;&#x6570;&#x636e;&#xff0c;cf1&#x662f;&#x6c47;&#x603b;&#x6570;&#x636e;&#xff0c;cf2&#x662f;&#x660e;&#x7ec6;&#x6570;&#x636e;"/>
+<node CREATED="1543570246651" ID="ID_284540093" MODIFIED="1543570259504" TEXT="&#x6570;&#x636e;&#x5e93;&#x76f4;&#x8054;&#x53c2;&#x6570;&#xff0c;json"/>
+<node CREATED="1543570260835" ID="ID_1669666236" MODIFIED="1543570282855" TEXT="&#x6a21;&#x677f;&#x683c;&#x5f0f;&#xff0c;&#x8868;&#x548c;&#x5b57;&#x6bb5;&#x5bf9;&#x5e94;&#x5173;&#x7cfb;&#xff0c;json"/>
+<node CREATED="1543570284317" ID="ID_1710467365" MODIFIED="1543570297078" TEXT="&#x5bfc;&#x5165;&#x53c2;&#x6570;&#xff0c;&#x5bfc;&#x5165;&#x671f;&#x95f4;"/>
+</node>
+<node CREATED="1543570307388" ID="ID_301072779" MODIFIED="1543570314767" TEXT="&#x547d;&#x4ee4;&#x884c;&#x6a21;&#x5f0f;"/>
+<node CREATED="1543570316040" ID="ID_621889108" MODIFIED="1543570325279" TEXT="Application&#x6a21;&#x5f0f;"/>
+</node>
+<node CREATED="1543570182820" ID="ID_1004921506" MODIFIED="1543570188275" TEXT="&#x5bfc;&#x5165;&#x6a21;&#x677f;&#x8bbe;&#x8ba1;">
+<node CREATED="1543570159908" ID="ID_1823676022" MODIFIED="1543570162168" TEXT="&#x884c;&#x653f;&#x96b6;&#x5c5e;&#x5173;&#x7cfb;&#xff1a;&#x5305;&#x542b;&#xff0c;&#x4e0d;&#x4e00;&#x5b9a;&#x6709;&#x5173;&#x7cfb;"/>
+<node CREATED="1543570093046" ID="ID_800745661" MODIFIED="1543570113348" TEXT="&#x91d1;&#x878d;&#x6570;&#x636e;&#x5bfc;&#x5165;&#x6a21;&#x677f;&#xff0c;rowkey&#xff1a;&#x8eab;&#x4efd;&#x8bc1;&#x53f7;&#x5012;&#x5e8f;&#xff0c;&#x65e5;&#x671f;&#xff0c;cf1&#xff1a;&#x6c47;&#x603b;&#x6570;&#x636e;-&gt;&#x5bf9;&#x624b;&#x8eab;&#x4efd;&#x8bc1;&#x53f7;,&#x8f6c;&#x5165;&#x8d44;&#x91d1;&#xff08;&#x4e07;&#x5143;&#xff09;&#xff0c;cf2&#xff1a;&#x660e;&#x7ec6;-&gt;&#x65f6;&#x95f4;&#xff0c;&#x4ea4;&#x6613;&#x91d1;&#x989d;&#xff0c;&#x65f6;&#x95f4;&#x6233;&#xff0c;&#x4f59;&#x989d;&#xff0c;&#x4ea4;&#x6613;&#x7801;&#xff0c;&#x6458;&#x8981;"/>
+<node CREATED="1543570120887" ID="ID_954165125" MODIFIED="1543570123578" TEXT="&#x5de5;&#x5546;&#x767b;&#x8bb0;&#x6ce8;&#x518c;&#x5bfc;&#x5165;&#x6a21;&#x677f;"/>
+<node CREATED="1543570131013" ID="ID_1274589041" MODIFIED="1543570133648" TEXT="&#x6c11;&#x653f;&#x5a5a;&#x59fb;&#x767b;&#x8bb0;&#x5bfc;&#x5165;&#x6a21;&#x677f;"/>
+<node CREATED="1543570141755" ID="ID_856711218" MODIFIED="1543570143672" TEXT="&#x52b3;&#x52a8;&#x5173;&#x7cfb;&#xff1a;&#x662f;&#x5426;&#x53ef;&#x4ee5;&#x5f52;&#x7c7b;&#x4e3a;&#x5546;&#x4e1a;&#x5408;&#x4f5c;&#xff0c;&#x56e0;&#x4e3a;&#x51fa;&#x5356;&#x7684;&#x662f;&#x52b3;&#x52a8;&#x529b;&#xff1f;"/>
+<node CREATED="1543570151037" ID="ID_235316649" MODIFIED="1543570152975" TEXT="&#x5730;&#x7406;&#x533a;&#x57df;&#x5173;&#x7cfb;&#xff1a;&#x5305;&#x542b;&#xff0c;&#x4e0d;&#x4e00;&#x5b9a;&#x6709;&#x5173;&#x7cfb;"/>
+<node CREATED="1543508993970" ID="ID_1400511938" MODIFIED="1543509012372" TEXT="&#x5546;&#x4e1a;&#x5408;&#x4f5c;&#xff1a;&#x7532;&#x65b9;&#x4e59;&#x65b9;&#x5173;&#x7cfb;"/>
+</node>
+</node>
+<node CREATED="1543508959207" ID="ID_1592955719" MODIFIED="1543508968776" POSITION="right" TEXT="&#x524d;&#x53f0;&#x6570;&#x636e;&#x5c55;&#x793a;"/>
+<node CREATED="1543570339214" ID="ID_375315016" MODIFIED="1543570356759" POSITION="left" TEXT="&#x4ece;HBASE&#x63d0;&#x53d6;&#x6570;&#x636e;&#x8fdb;&#x5165;NEO4J"/>
+</node>
+</map>
