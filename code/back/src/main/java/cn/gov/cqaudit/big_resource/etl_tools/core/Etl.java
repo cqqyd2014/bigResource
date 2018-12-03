38140e62b6684a47cc40b4636e373db1e1eb433c
cqqyd2014 <cqqyd2014@hotmail.com>
2018-12-03 19:07:20 +0800

    20181203001

---
+++ code/back/src/main/java/cn/gov/cqaudit/big_resource/etl_tools/core/Etl.java
@@ -0,0 +1,7 @@
+package cn.gov.cqaudit.big_reource.etl_tools.core;
+
+public class Etl {
+  public boolean someMethod(){
+    return true;
+  }
+}
---
+++ code/back/src/test/java/TestApp.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class TestApp {
+
+     @Test public void testSomeLibraryMethod() {
+       System.out.println("sdfsdf");
+
+        assertTrue("someLibraryMethod should return 'true'", true);
+    }
+}
