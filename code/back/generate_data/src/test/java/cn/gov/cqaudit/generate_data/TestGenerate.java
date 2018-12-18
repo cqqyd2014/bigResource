069b8d5d37f96466fd8e7333985bc256f52bb3d9
cqqyd2014 <cqqyd2014@hotmail.com>
2018-12-18 20:05:03 +0800

    20181218200400

--- code/back/generate_data/src/test/java/cn/gov/cqaudit/generate_data/TestGenerate.java
+++ code/back/generate_data/src/test/java/cn/gov/cqaudit/generate_data/TestGenerate.java
@@ -1,21 +1,17 @@
 package cn.gov.cqaudit.generate_data;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import org.junit.jupiter.api.*;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
 public class TestGenerate{
 
   @Test public void testGenerateData() {
 
-	  GenerateString g=new GenerateString();
-	  cn.gov.cqaudit.tools.Province_city pc=new cn.gov.cqaudit.tools.Province_city();
-     assertEquals("a", "a");
  }
- @BeforeAll
-    static void initAll() {
-    }
 
-    @BeforeEach
-    void init() {
-    }
 
 }
