277b49d7e9568ea0ce1f9bd2c8f1c3900cdae7ed
cqqyd2014 <cqqyd2014@hotmail.com>
2018-12-20 00:00:06 +0800

    20181219000000

--- code/back/import_hbase_module/src/main/java/cn/gov/cqaudit/big_resource/import_hbase_module/BootApplication.java
+++ code/back/import_hbase_module/src/main/java/cn/gov/cqaudit/big_resource/import_hbase_module/BootApplication.java
@@ -6,13 +6,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.hadoop.fs.FsShell;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.test.context.junit4.*;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
 @SpringBootApplication
 public class BootApplication implements CommandLineRunner {
 
