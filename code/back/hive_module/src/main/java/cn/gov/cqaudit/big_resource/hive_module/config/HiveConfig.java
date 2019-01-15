b594f83a6598bce76336c6a6616eb0e2e3236726
王利 <wangli@wl-imac.local>
2019-01-15 22:59:32 +0800

    20190115105900

--- code/back/hive_module/src/main/java/cn/gov/cqaudit/big_resource/hive_module/config/HiveConfig.java
+++ code/back/hive_module/src/main/java/cn/gov/cqaudit/big_resource/hive_module/config/HiveConfig.java
@@ -1,13 +1,16 @@
 package cn.gov.cqaudit.big_resource.hive_module.config;
 
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.data.hadoop.hive.HiveClientFactory;
-import org.springframework.data.hadoop.hive.HiveClientFactoryBean;
-import org.springframework.data.hadoop.hive.HiveRunner;
+import org.springframework.jdbc.core.JdbcTemplate;
+
 
 @Configuration
 @PropertySource(value = { "classpath:hive.properties" })
@@ -18,6 +21,25 @@ public class HiveConfig {
 		// TODO Auto-generated constructor stub
 		System.out.println("初始化Hive Config");
 	}
+	/*
+	@Bean(name = "hive_datasource")
+	 
+	 public DataSource dataSource(@Value("${hive.url}") String hive_url){
+			 DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
+			 
+			 dataSourceBuilder.driverClassName("org.apache.hive.jdbc.HiveDriver");
+			 
+			 dataSourceBuilder.url(hive_url);
+			 
+			 return dataSourceBuilder.build();
+	 }
+	 @Bean(name = "hiveJdbcTemplate")
+	 
+	 public JdbcTemplate jdbcTemplate(@Qualifier("hive_datasource") DataSource dataSource){
+			 return new JdbcTemplate(dataSource);
+	 }
+*/
+	
 	@Bean(name = "hiveDataSource")
 	public org.springframework.jdbc.datasource.SimpleDriverDataSource getDataSource(@Value("${hive.url}") String hive_url) {
 		org.springframework.jdbc.datasource.SimpleDriverDataSource dataSource = new org.springframework.jdbc.datasource.SimpleDriverDataSource(new org.apache.hive.jdbc.HiveDriver(),hive_url);
@@ -25,7 +47,7 @@ public class HiveConfig {
 		dataSource.setDriver(new org.apache.hive.jdbc.HiveDriver());
 		dataSource.setUrl(hive_url);
 
-		
+
 		return dataSource;
 	}
 	@Bean(name = "hiveClientFactory")
@@ -35,12 +57,12 @@ public class HiveConfig {
 
 		return hiveClientFactory.getObject();
 	}
-	
+
 
 	@Bean(name = "hiveRunner")
 	public HiveRunner getHiveRunner(HiveClientFactory hiveClientFactory) {
 		HiveRunner hiveRunner = new HiveRunner();
-		hiveRunner.setHiveClientFactory(hiveClientFactory); 
+		hiveRunner.setHiveClientFactory(hiveClientFactory);
 
 		return hiveRunner;
 	}
--- code/back/hive_module/src/test/java/cn/gov/cqaudit/big_resource/hive_module/TestApp.java
+++ code/back/hive_module/src/test/java/cn/gov/cqaudit/big_resource/hive_module/TestApp.java
@@ -4,14 +4,19 @@ package cn.gov.cqaudit.big_resource.hive_module;
 
 import java.io.IOException;
 
+import javax.annotation.Resource;
+
 import org.apache.hadoop.hbase.client.Connection;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.junit4.*;
 
+
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.hadoop.hive.HiveClientFactory;
+
+import org.springframework.jdbc.core.JdbcTemplate;
 
 
 
@@ -20,12 +25,17 @@ import org.springframework.data.hadoop.hive.HiveClientFactory;
 @SpringBootTest(classes=HiveModuleBoot.class)
 public class TestApp {
 
+	/*
+	@Resource(name="hiveJdbcTemplate")        //实现类1中 @Service注解中标定的名称
+    private JdbcTemplate hiveJdbcTemplate;
+	 */
 	
 	@Autowired
-	private HiveClientFactory hiveClientFactory;
-	 
+	HiveClientFactory hiveClientFactory;
      @Test
      public void testSome() {
+    	 
+    	
     	
 		
 		
