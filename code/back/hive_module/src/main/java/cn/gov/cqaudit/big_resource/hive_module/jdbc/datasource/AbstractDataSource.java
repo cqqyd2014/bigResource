d7d7f50e449fd43973b239107466a15b47f6636e
王利 <wangli@wl-imac.local>
2019-01-16 23:00:07 +0800

    201901623000

---
+++ code/back/hive_module/src/main/java/cn/gov/cqaudit/big_resource/hive_module/jdbc/datasource/AbstractDataSource.java
@@ -0,0 +1,78 @@
+package cn.gov.cqaudit.big_resource.hive_module.jdbc.datasource;
+
+import java.io.PrintWriter;
+import java.sql.SQLException;
+import java.util.logging.Logger;
+
+import javax.sql.DataSource;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+public abstract class AbstractDataSource  implements DataSource {
+	/** Logger available to subclasses. */
+	protected final Log logger = LogFactory.getLog(getClass());
+
+
+	/**
+	 * Returns 0, indicating the default system timeout is to be used.
+	 */
+	@Override
+	public int getLoginTimeout() throws SQLException {
+		return 0;
+	}
+
+	/**
+	 * Setting a login timeout is not supported.
+	 */
+	@Override
+	public void setLoginTimeout(int timeout) throws SQLException {
+		throw new UnsupportedOperationException("setLoginTimeout");
+	}
+
+	/**
+	 * LogWriter methods are not supported.
+	 */
+	@Override
+	public PrintWriter getLogWriter() {
+		throw new UnsupportedOperationException("getLogWriter");
+	}
+
+	/**
+	 * LogWriter methods are not supported.
+	 */
+	@Override
+	public void setLogWriter(PrintWriter pw) throws SQLException {
+		throw new UnsupportedOperationException("setLogWriter");
+	}
+
+
+	//---------------------------------------------------------------------
+	// Implementation of JDBC 4.0's Wrapper interface
+	//---------------------------------------------------------------------
+
+	@Override
+	@SuppressWarnings("unchecked")
+	public <T> T unwrap(Class<T> iface) throws SQLException {
+		if (iface.isInstance(this)) {
+			return (T) this;
+		}
+		throw new SQLException("DataSource of type [" + getClass().getName() +
+				"] cannot be unwrapped as [" + iface.getName() + "]");
+	}
+
+	@Override
+	public boolean isWrapperFor(Class<?> iface) throws SQLException {
+		return iface.isInstance(this);
+	}
+
+
+	//---------------------------------------------------------------------
+	// Implementation of JDBC 4.1's getParentLogger method
+	//---------------------------------------------------------------------
+
+	@Override
+	public Logger getParentLogger() {
+		return Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
+	}
+}
